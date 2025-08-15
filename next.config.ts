4fe0c02f1fae6bbfa66efec4202bac93920dada9
Nebula Sync Robotics <nebula.sync.robotics@gmail.com>
2025-08-15 13:59:46 +0000

    images

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
-    unoptimized: true
+    unoptimized: true,
+    loader: "akamai", path: "/",
   },
   basePath: 'nebula-sync-robotics-web-site' // remove this in development 
+  
 };
 
 export default nextConfig;
